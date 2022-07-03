a8312d05ab01be730b9bf1db3a05ed6c1e1992d5
ozlemts <ozlemturkerii@gmail.com>
2022-07-03 19:48:46 +0200

    Companies state has consumed by CompanyTable

--- src/CompanyTable.tsx
+++ src/CompanyTable.tsx
@@ -1,21 +1,12 @@
 import React from "react";
+import {CompaniesContext} from "./companiesContext";
 
 export const CompanyTable = () => {
-  const data: Array<companyType> = [
-    {"id": 145, "name": "Google"},
-    {"id": 496, "name": "Amazon"},
-    {"id": 129, "name": "Facebook"},
-    {"id": 593, "name": "Apple"},
-    {"id": 952, "name": "Samsung"},
-    {"id": 819, "name": "Microsoft"},
-    {"id": 427, "name": "Tesla"},
-    {"id": 901, "name": "Alibaba"}
-  ]
-
+  const { companyList} = React.useContext(CompaniesContext) as CompanyContextType;
   return (
     <div>
       <ul>
-        {data.map((e) => (
+        {companyList.map((e) => (
           <li>{e.name}</li>
         ))}
       </ul>
